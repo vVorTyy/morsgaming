fa1274e6b6e323b8c0aff4fc2fdb8f149031f42a
vVorTyy <alabdali1333@gmail.com>
2024-09-10 02:58:33 +0300

    modified on App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Nav/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/morsgaming/" element={<Home/>}/>
           <Route path="/about" element={<About/>} />
           <Route path="/blog" element={<Blog/>} />
           <Route path='/contact' element={<Contact/>}/>
